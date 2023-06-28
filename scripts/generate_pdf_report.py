5097435d69e028a7b411052ec89fce8772c0a0dc
amitd3445 <amitd3445@gmail.com>
2023-06-27 22:52:38 -0400

    job fitment

--- scripts/generate_pdf_report.py
+++ scripts/generate_pdf_report.py
@@ -5,11 +5,14 @@ import os
 import datetime as dt
 from PIL import Image
 import pandas as pd
+import numpy as np
 import matplotlib
+import matplotlib.colors as mcolors
 
 matplotlib.use("Agg")
 from matplotlib import pyplot as plt
 from jinja2 import Environment, FileSystemLoader
+#import weasyprint
 
 
 def generate_interview_report(
@@ -34,11 +37,17 @@ def generate_interview_report(
     """
 
     _validate_payload(payload)
-    dict_candidate, df_all_scores, list_series_agent_scores = _parse_payload(payload)
+    (
+        dict_candidate,
+        dict_job_fitment,
+        df_all_scores,
+        list_series_agent_scores,
+    ) = _parse_payload(payload)
+    _generate_job_fitment_bar(dict_job_fitment)
     _generate_gauge_charts(df_all_scores["Self"])
     _generate_spider_plot(*list_series_agent_scores)
     _generate_final_report(dict_candidate, df_all_scores["Self"])
-    _delete_temp_files()
+    #_delete_temp_files()
 
 
 def _validate_payload(payload: Dict[str, Dict[str, Union[float, int, str]]]) -> None:
@@ -94,10 +103,148 @@ def _parse_payload(
         Tuple[Dict[str, Union[float, str]]]: a tuple that breaks down the payload into discrete inputs for future functions
     """
     dict_candidate = payload.pop("Candidate")
+    dict_job_fitment = payload.pop("Job Fitment")
     payload = {key.lower(): value for key, value in payload.items()}
     df_all_scores = pd.DataFrame(payload).transpose()
     list_series_agent_scores = [df_all_scores[col] for col in df_all_scores.columns]
-    return (dict_candidate, df_all_scores, list_series_agent_scores)
+    return (dict_candidate, dict_job_fitment, df_all_scores, list_series_agent_scores)
+
+
+def _generate_job_fitment_bar(dict_scores: Dict[str, Union[float, int]]) -> None:
+    """
+    Creates horizontal gauge charts based on the individual's scores.
+
+    Args:
+        param(Dict[str, Dict[str, Union[float, int]]]): a nested dictionary that corresponds
+        to the score receieved for each focus area/skill
+
+    Returns:
+        None
+    """
+
+    R1, R2, score = dict_scores["R1"], dict_scores["R2"], dict_scores["S"]
+
+    fig = plt.figure(figsize=(8, 2))
+    ax = fig.add_axes([0.1, 0.35, 0.8, 0.4])
+    ax2 = fig.add_axes([0.1, 0.1, 0.8, 0.25])
+    ax3 = fig.add_axes([0.1, 0.7, 0.8, 0.1])
+
+    left_color = "#FCBEC1"
+    right_color = "#D9FBC8"
+    center_color = "#F4F4F4"
+
+    # Create a colormap for the left half (green to white)
+    cmap_left = mcolors.LinearSegmentedColormap.from_list(
+        "LeftCmap", [left_color, center_color]
+    )
+
+    # Create a colormap for the right half (white to red)
+    cmap_right = mcolors.LinearSegmentedColormap.from_list(
+        "RightCmap", [center_color, right_color]
+    )
+
+    # white cmap
+    cmap_white = mcolors.LinearSegmentedColormap.from_list(
+        "WhiteCmap", ["white", "white"]
+    )
+
+    # Combine the left and right colormaps
+    colors = np.vstack(
+        (cmap_left(np.linspace(0, 1, 256)), cmap_right(np.linspace(0, 1, 256)))
+    )
+    cmap_custom = mcolors.ListedColormap(colors)
+
+    guage_range = np.linspace(0, 100, 512)
+
+    norm = matplotlib.colors.Normalize(vmin=guage_range[0], vmax=guage_range[-1])
+
+    cbar = matplotlib.colorbar.ColorbarBase(
+        ax,
+        cmap=cmap_custom,
+        norm=norm,
+        orientation="horizontal",
+        boundaries=guage_range,
+    )
+
+    cbar2 = matplotlib.colorbar.ColorbarBase(
+        ax2,
+        cmap=cmap_white,
+        norm=norm,
+        orientation="horizontal",
+        boundaries=guage_range,
+    )
+
+    cbar3 = matplotlib.colorbar.ColorbarBase(
+        ax3,
+        cmap=cmap_white,
+        norm=norm,
+        orientation="horizontal",
+        boundaries=guage_range,
+    )
+
+    cbar.outline.set_visible(False)
+    cbar2.outline.set_visible(False)
+    cbar3.outline.set_visible(False)
+
+    # R1 to R2 grey haze
+    ax.axvspan(R1, R2, 0, 1, facecolor="#ECECEC")
+    ax2.axvspan(R1, R2, 0.85, 1, facecolor="#ECECEC")
+    ax3.axvspan(R1, R2, 0, 0.3, facecolor="#ECECEC")
+
+    # score tick
+    ax.axvspan(score - 0.5, score + 0.5, 0, 1, facecolor="#000000")
+    ax2.axvspan(score - 0.5, score + 0.5, 0.6, 1, facecolor="#000000")
+    ax3.axvspan(score - 0.5, score + 0.5, 0, 1, facecolor="#000000")
+
+    path_skill_gauge_chart = (
+        pathlib.Path(__file__).parent.parent / "tmp" / "job_fitment_graphic.jpg"
+    )
+
+    ax.set_xticks([])
+    ax2.set_xticks([])
+    ax3.set_xticks([])
+
+    annotation = plt.annotate(
+        "0%", xy=(0.1, 0.15), xycoords="figure fraction", ha="center", fontsize=10
+    )
+    annotation2 = plt.annotate(
+        "100%",
+        xy=(0.9, 0.15),
+        xycoords="figure fraction",
+        ha="center",
+        fontsize=10,
+    )
+    annotation3 = plt.annotate(
+        str(score) + "%",
+        xy=(score / 125 + 0.1, 0.85),
+        xycoords="figure fraction",
+        ha="center",
+        fontsize=10,
+    )
+
+    annotation4 = plt.annotate(
+        "R1",
+        xy=(R1 / 125 + 0.1, 0.15),
+        xycoords="figure fraction",
+        ha="center",
+        fontsize=10,
+    )
+
+    annotation5 = plt.annotate(
+        "R2",
+        xy=(R2 / 125 + 0.1, 0.15),
+        xycoords="figure fraction",
+        ha="center",
+        fontsize=10,
+    )
+
+    plt.savefig(path_skill_gauge_chart, format="jpg")
+
+    annotation.remove()
+    annotation2.remove()
+    annotation3.remove()
+    annotation4.remove()
+    annotation5.remove()
 
 
 def _generate_gauge_charts(series_scores: pd.Series) -> None:
@@ -127,11 +274,7 @@ def _generate_gauge_charts(series_scores: pd.Series) -> None:
 
     for category in series_scores.index:
         category_string = str(category) + ".jpeg"
-        path_category = (
-            pathlib.Path(__file__).parent.parent
-            / "tmp"
-            / category_string
-        )
+        path_category = pathlib.Path(__file__).parent.parent / "tmp" / category_string
 
         plt.figure(figsize=(10, 10))
         ax = plt.subplot(1, 1, 1, polar=True)
@@ -247,9 +390,7 @@ def _generate_spider_plot(*args: pd.Series) -> None:
     )
 
     path_spiderplot_graph = (
-        pathlib.Path(__file__).parent.parent
-        / "tmp"
-        / "baseline_assessment.jpg"
+        pathlib.Path(__file__).parent.parent / "tmp" / "baseline_assessment.jpg"
     )
     plt.savefig(path_spiderplot_graph, format="jpg")
 
@@ -265,8 +406,10 @@ def _choose_skills_for_spider_plot(series_self_score: pd.Series) -> List[str]:
         List[str]: list of skills/categories selected
     """
 
-    return series_self_score.sample(n=10).index
-
+    series_sorted = series_self_score.sort_values(ascending=True)
+    list_bottom_skills = series_sorted[:5].index.to_list()
+    list_top_skills = series_sorted[-5:].index.to_list()
+    return list_bottom_skills + list_top_skills
 
 def _generate_final_report(
     dict_candidate: Dict[str, str], series_self_score: pd.Series
@@ -282,6 +425,7 @@ def _generate_final_report(
         None
     """
     _generate_html(dict_candidate, series_self_score)
+    #_generate_pdf(dict_candidate)
 
 
 def _generate_html(
@@ -323,9 +467,16 @@ def _generate_html(
     }
 
     rendered_template = template.render(payload)
+    
+    name, company = dict_candidate["name"].replace(" ", "_"), dict_candidate[
+        "company_name"
+    ].replace(" ", "_")
+    date_today_string = dt.date.today().strftime("%Y-%m-%d")
+    report_filename = "_".join([name, company, date_today_string])
+    report_filename += ".html"
 
     path_rendered_template = (
-        pathlib.Path(__file__).parent.parent / "templates" / "rendered_template.html"
+        pathlib.Path(__file__).parent.parent / "results" / report_filename
     )
 
     with open(path_rendered_template, "w") as file:
@@ -387,6 +538,32 @@ def _get_text_for_top_and_bottom_skills(
     return dict_top_bottom_skills
 
 
+def _generate_pdf(dict_candidate: Dict[str, str]) -> None:
+    """
+    Creates the final PDF file and saves to the results folder
+
+    Args:
+        param1(Dict[str, int | str]]): The candidate's profile
+
+    Returns:
+        None
+    """
+    name, company = dict_candidate["name"].replace(" ", "_"), dict_candidate[
+        "company_name"
+    ].replace(" ", "_")
+    date_today_string = dt.date.today().strftime("%Y-%m-%d")
+    report_filename = "_".join([name, company, date_today_string])
+    report_filename_html = report_filename + ".html"
+    report_filename_pdf = report_filename + '.pdf'
+
+    path_html_file = (
+        pathlib.Path(__file__).parent.parent / "results" / report_filename_html
+    )
+    path_pdf_report = pathlib.Path(__file__).parent.parent / "results" / report_filename_pdf
+
+    weasyprint.HTML(path_html_file).write_pdf(path_pdf_report)
+
+
 def _delete_temp_files() -> None:
     """
     Deletes all files that were created except for the PDF file (images/graphs and html/css)
@@ -416,7 +593,8 @@ def _delete_temp_files() -> None:
 
 if __name__ == "__main__":
     payload = {
-        "Candidate": {"name": "John Doe", "company": "COMPANY_NAME"},
+        "Candidate": {"name": "John Doe", "company_name": "COMPANY_NAME"},
+        "Job Fitment": {"R1": 40, "R2": 80, "S": 80},
         "Achievement orientation": {"Self": 8, "Comparison": 3},
         "Adaptability": {"Self": 6, "Comparison": 9},
         "Attention to detail": {"Self": 2, "Comparison": 7},
